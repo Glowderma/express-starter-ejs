eee9637441a99d691f11aa2d2ac80c03bc43826c
Ashik Khatri <ashikrk@gmail.com>
2023-05-23 01:55:27 -0700

    added eslint

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 const indexRouter = express.Router();
 
 /* GET home page. */
-indexRouter.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+indexRouter.get('/', function(req, res) {
+    res.render('index', { title: 'Express' });
 });
 
 export default indexRouter;
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,11 +1,11 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const getDirName = function (moduleUrl) {
-    const filename = fileURLToPath(moduleUrl)
-    return path.dirname(filename)
-}
+    const filename = fileURLToPath(moduleUrl);
+    return path.dirname(filename);
+};
 
 export {
     getDirName
-}
+};
