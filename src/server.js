947ad1a68406dacf9e06fcdd04d3b20a1ea3df38
Ashik Khatri <ashikrkk@gmail.com>
2024-02-18 15:31:21 -0800

    added termination handler to server

--- src/server.js
+++ src/server.js
@@ -2,10 +2,25 @@ import app from './app.js';
 import {api} from "./utils/env.js";
 import logger from "./utils/logger.js";
 
-app.listen(api.port, (err) => {
+const server = app.listen(api.port, (err) => {
     if (err) {
         logger.error(err);
     } else {
         logger.info(`Successfully launched in ${api.nodeEnv} environment on port ${api.port}`);
     }
 });
+
+function terminationHandler(signal) {
+    logger.info(`Received ${signal} signal`);
+    server.close((err) => {
+        if(err) {
+            logger.error(`Terminated with error: ${err}`);
+        } else {
+            logger.info('Terminated without error');
+        }
+    });
+}
+
+process.on('SIGINT', terminationHandler);
+process.on('SIGTERM', terminationHandler);
+process.on('SIGQUIT', terminationHandler);
